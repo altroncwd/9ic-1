69b3dd65acff1e2bb1fe11343dd2edbcf2124796
christopher decker <christopher.w.decker@gmail.com>
2015-12-30 14:46:47 -0800

    This change will: correctly remove test obj and prior notes

--- src/components/Tinder/Tinder.js
+++ src/components/Tinder/Tinder.js
@@ -2,22 +2,8 @@ import React,{Component} from 'react'
 import apiTMDB from '../../utils/apiTMDB'
 
 /*==========  NOTE  =========== 
-12/30/15 > tempData is a placeholder untill I can call on the actual DB
-12/30/15 > need to get access to the api to grab a random movie;
+12/30/15 > need to get the genres and posters properly;
 */
-
-const tempData= {
-  title: "Lego Batman Movie",
-  genre: ['Animation', 'Action', 'Adventure', 'Comedy', 'Family', 'Fantasy'],
-  summary: "Its the lego movie what more can I say about how great the movie is.",
-  rating: 'PG',
-  cover: 'http://cdn.wegotthiscovered.com/wp-content/uploads/lego-will-arnett-batman-600.jpg'
-}
-
-console.log("testing out an api call");
-apiTMDB.getMovies()
-
-
 
 export default class Tinder extends Component {
   constructor(props) {
