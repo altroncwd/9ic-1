9e8492330567b2c9f8719f748d47c77be3496607
christopher decker <christopher.w.decker@gmail.com>
2015-12-30 12:09:25 -0800

    this commit will: add the Tinder route

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,11 +4,14 @@ import Home from '../components/MainPage/Home';
 import Main from '../components/MainPage/Main';
 import Movie from '../components/Movie/Movie';
 import GenreList from '../components/Genre/GenreList';
+import Tinder from '../components/Tinder/Tinder';
 
 export default (
   <Route path="/" component={Main}>
     <Route path="movie/:id" component={Movie} />
     <Route path="genre/" component={GenreList} />
+    <Route path="tinder/" component={Tinder} />
+
     <IndexRoute component={Home}/>
   </Route>
 );
