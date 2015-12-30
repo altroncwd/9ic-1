b9c39bed3470b32ec116fff232ccb9d4daea312c
christopher decker <christopher.w.decker@gmail.com>
2015-12-30 12:45:42 -0800

    This commit will: add a placeholder and notes for a nessisary funciton

--- src/components/Tinder/Tinder.js
+++ src/components/Tinder/Tinder.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react'
 
 /*==========  NOTE  =========== 
-12/29/15 > tempData is a placeholder untill I can call on the actual DB
+12/30/15 > tempData is a placeholder untill I can call on the actual DB
+12/30/15 > need to get access to the api to grab a random movie;
 */
 
 const tempData= {
@@ -11,6 +12,17 @@ const tempData= {
   rating: 'PG',
   cover: 'http://cdn.wegotthiscovered.com/wp-content/uploads/lego-will-arnett-batman-600.jpg'
 }
+
+const getOneMovie = () => {
+  // we want this function to call the api and retreave a random movie.
+
+
+  // we should expect to get one object, or a short list of movies 
+  // list may or maynot be ordered, if ordered, the list will be deligated to another function
+}
+
+
+
 
 export default class Tinder extends Component {
   render(){
