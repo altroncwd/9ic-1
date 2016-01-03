dbc3cc3e26bb9e9f7a3f718451b87567b93fbf26
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-03 12:50:21 -0800

    save new user data in database

--- src/components/LikeButtonsWidget.js
+++ src/components/LikeButtonsWidget.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { saveMovieData } from '../helpers/firebaseHelpers'
 
-export default () => {
+export default ({title, popularity, overview, poster_path, id}) => {
   return (
    <div className="like-buttons">
-       <a href="#" className="btn btn-success">Like</a>
+    <a href="#" onClick= { () => {
+      saveMovieData(title, popularity, overview, poster_path, id)
+    }
+    } className="btn btn-success">Like</a>
        <a href="#" className="btn btn-danger">Dislike</a>
    </div>
   )
--- src/helpers/firebaseHelpers.js
+++ src/helpers/firebaseHelpers.js
@@ -2,14 +2,21 @@ var Firebase = require('firebase')
 
 export const firebaseRef = new Firebase('https://movtin.firebaseio.com')
 
+
+export const getAuth = firebaseRef.getAuth()
+
 export const facebookLoginPopUp = () => {
   firebaseRef.authWithOAuthPopup('facebook', (error, authData) => {
     if (error) {
       console.log('Login Failed!', error)
     } else {
-      console.log('Authenticated successfully with payload:', authData)
+      firebaseRef.child('users').child(authData.uid).set({
+        provider: authData.provider,
+        name: authData.facebook.displayName
+      })
     }
   })
 }
-
-export const getAuth = firebaseRef.getAuth()
+export const saveMovieData = (title, popularity, overview, poster_path, id) => {
+  
+}
