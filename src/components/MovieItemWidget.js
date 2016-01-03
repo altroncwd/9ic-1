261365d0eb174fc33d719827f3d8bda3fc12d6a1
christopher decker <christopher.w.decker@gmail.com>
2016-01-02 19:06:57 -0800

    This change will: fix the Crumb widget

--- src/components/MovieItemWidget.js
+++ src/components/MovieItemWidget.js
@@ -8,8 +8,8 @@ import AddReviewFormWidget from './AddReviewFormWidget'
 export default ({ movie }) => {
   return (
     <div className="movie-item">
-        <CrumbWidget/>
-        <MovieItemHeaderWidget {...movie} {...movie} />
+        <CrumbWidget {...movie} />
+        <MovieItemHeaderWidget {...movie} />
         <MovieItemBodyWidget {...movie} />
         <ReviewListWidget {...movie} />
         <AddReviewFormWidget {...movie} />
