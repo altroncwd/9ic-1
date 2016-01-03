fcc9fabcee53a5f611cacc86c6df8debd3ebb86a
Ahmed Modan <ahmed.modan@gmail.com>
2016-01-03 14:19:04 -0800

    make like and dislike buttons be able to send data to firebase database

--- src/components/LikeButtonsWidget.js
+++ src/components/LikeButtonsWidget.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { saveMovieData } from '../helpers/firebaseHelpers'
 
 export default ({title, popularity, overview, poster_path, id}) => {
+  let saveData = (preference) => {
+    return () => saveMovieData({ title, popularity, overview, poster_path, id }, preference)
+  }
   return (
    <div className="like-buttons">
-    <a href="#" onClick= { () => {
-      saveMovieData(title, popularity, overview, poster_path, id)
-    }
-    } className="btn btn-success">Like</a>
-       <a href="#" className="btn btn-danger">Dislike</a>
+    <a href="#" onClick={saveData('like')} className="btn btn-success">Like</a>
+       <a href="#" onClick={saveData('dislike')} className="btn btn-danger">Dislike</a>
    </div>
   )
 }
